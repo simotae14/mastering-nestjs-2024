cdaec88346cd3a738bfbfd89afcd39696ce13901
Simone Taeggi <startae14@gmail.com>
2025-05-07 23:34:50 +0200

    define a controller path and get path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('products')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('get')
   getHello(): string {
     return this.appService.getHello();
   }
