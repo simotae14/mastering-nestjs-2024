c182be026c943d50dc884e6199988fa94aa078e8
Simone Taeggi <startae14@gmail.com>
2025-05-08 23:34:34 +0200

    Fetching Param and Query Params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,17 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get(':id')
-  fetchReq(@Req() req: Request, @Res() res: Response) {
-    const { id } = req.params;
-    const queryParams = req.query;
-    const userAgent = req.headers['user-agent'];
-
-    return res.send(`
-        <script>
-            console.log("Id: ", "${id}");
-            console.log("Query Params: ", ${JSON.stringify(queryParams)});
-            console.log("User Agent: ", "${userAgent}");
-        </script>    
-    `);
+  fetchQuery(
+    @Param('id') id: string,
+    @Query('name') name: string,
+    @Query('age') age: number,
+  ) {
+    return {
+      Id: `${id}`,
+      Name: `${name}`,
+      Age: `${age}`,
+    };
   }
 }
