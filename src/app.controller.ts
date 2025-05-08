06c3f867b7238ccf6a121ad3df7342b39d84d601
Simone Taeggi <startae14@gmail.com>
2025-05-08 23:20:18 +0200

    use Response Object

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Get, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   @Get(':id')
-  fetchReq(@Req() req: Request) {
+  fetchReq(@Req() req: Request, @Res() res: Response) {
     const { id } = req.params;
     const queryParams = req.query;
     const userAgent = req.headers['user-agent'];
 
-    return {
-      id,
-      queryParams,
-      userAgent,
-    };
+    return res.send(`
+        <script>
+            console.log("Id: ", "${id}");
+            console.log("Query Params: ", ${JSON.stringify(queryParams)});
+            console.log("User Agent: ", "${userAgent}");
+        </script>    
+    `);
   }
 }
