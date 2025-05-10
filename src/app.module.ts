6c330f8ea9065ee6a9fd6097f42c7ce460d50084
Simone Taeggi <startae14@gmail.com>
2025-05-10 22:13:53 +0200

    Implementing a middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
-import { ProductsService } from './products/products.service';
-import { ProductsModule } from './products/products.module';
-import { AuthController } from './auth/auth.controller';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController, ProductsController, AuthController],
-  providers: [AppService, ProductsService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
