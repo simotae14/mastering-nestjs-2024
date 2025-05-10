d6b365522cdde82c6af346e79a00d0987b47c9f2
Simone Taeggi <startae14@gmail.com>
2025-05-10 19:15:32 +0200

    Custom pipe Handling different type Arguments

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,11 +4,21 @@ import {
   Post,
   UsePipes,
   ValidationPipe,
+  Get,
+  Param,
 } from '@nestjs/common';
 import { CustomPipe } from './customPipe/customPipe';
 
 @Controller('auth')
 export class AuthController {
+  @Get('register/:id')
+  @UsePipes(new CustomPipe())
+  getId(@Param('id') id: string) {
+    return {
+      data: id,
+    };
+  }
+
   @Post('register')
   @UsePipes(new ValidationPipe(), CustomPipe)
   registerUser(@Body('name') name: string) {
--- src/auth/customPipe/customPipe.ts
+++ src/auth/customPipe/customPipe.ts
@@ -4,10 +4,15 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class CustomPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    if (metadata.type === 'param' && typeof value === 'string') {
+    if (metadata.type === 'body' && typeof value === 'string') {
       value = value.toUpperCase();
+    } else if (metadata.type === 'param') {
+      const idLength = parseInt(value as string, 10);
+      if (!isNaN(idLength) && idLength > 0) {
+        const randomId = Math.floor(Math.random() * Math.pow(10, idLength));
+        value = randomId;
+      }
     }
-
     return value;
   }
 }
