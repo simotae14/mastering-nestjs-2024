8eed57749f8b54debad17ef07dd3511c91f308b3
Simone Taeggi <startae14@gmail.com>
2025-05-10 22:41:06 +0200

    Registering a middleware

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Post()
+  createMsg(@Body() msg: string) {
+    console.log(msg);
+    return 'Message received successfully';
+  }
+}
--- src/middleware/logging.middleware.ts
+++ src/middleware/logging.middleware.ts
@@ -5,5 +5,7 @@ import { NextFunction, Request, Response } from 'express';
 export class LoggingMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log(`[${new Date().toISOString()}]`);
+
+    next();
   }
 }
