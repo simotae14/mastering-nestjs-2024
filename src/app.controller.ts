4baf408f0181f03e216cd280c17f2989ae35fc21
Simone Taeggi <startae14@gmail.com>
2025-05-10 11:53:11 +0200

    Built in pipes: ParseFloatPipe

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseFloatPipe } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get(':id')
-  getId(@Param('id', ParseIntPipe) id: number) {
+  getId(@Param('id', ParseFloatPipe) id: number) {
     return `Fetched Id: ${id}`;
   }
 }
